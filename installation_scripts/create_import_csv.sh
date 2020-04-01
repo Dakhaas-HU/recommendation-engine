a04e0b3231c4ab469ac3250761be1b1da1bac978
Nouw <offabio@outlook.com>
2020-04-01 19:21:55 +0200

    Fixed error where file couldn't be found

--- installation_scripts/create_import_csv.sh
+++ installation_scripts/create_import_csv.sh
@@ -6,9 +6,9 @@ python ../database/mongo_to_csv/create_order_csv.py
 python ../database/mongo_to_csv/create_viewed_brand_csv.py
 python ../database/mongo_to_csv/create_viewed_category_csv.py
 python ../database/mongo_to_csv/create_viewed_gender_csv.py
-python ../database/mongo_to_csv/create_viewed_sub_category.py
-python ../database/mongo_to_csv/create_viewed_sub_sub_category.py
-python ../database/mongo_to_csv/create_viewed_promos.py
-python ../database/mongo_to_csv/create_viewed_type.py
-python ../database/mongo_to_csv/create_viewed_product_type.py
-python ../database/mongo_to_csv/create_viewed_product_size.py
+python ../database/mongo_to_csv/create_viewed_sub_category_csv.py
+python ../database/mongo_to_csv/create_viewed_sub_sub_category_csv.py
+python ../database/mongo_to_csv/create_viewed_promos_csv.py
+python ../database/mongo_to_csv/create_viewed_type_csv.py
+python ../database/mongo_to_csv/create_viewed_product_type_csv.py
+python ../database/mongo_to_csv/create_viewed_product_size_csv.py

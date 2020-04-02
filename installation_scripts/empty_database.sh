44810316be1a581a745fd9d314826184f443f367
Nouw <offabio@outlook.com>
2020-04-02 16:30:51 +0200

    Redone the deletion script

--- installation_scripts/empty_database.sh
+++ installation_scripts/empty_database.sh
@@ -1,4 +1,5 @@
-mysql -ugroupproject -pBierkeet42069! --local_infile=1 huwebshop -e "DELETE FROM profiles; DELETE FROM previously_recommended; DELETE FROM viewed_before; DELETE FROM order;
+mysql -ugroupproject -pBierkeet42069! --local_infile=1 huwebshop -e "DELETE FROM previously_recommended; DELETE FROM viewed_before; DELETE FROM order;
 DELETE FROM viewed_brand; DELETE FROM viewed_category; DELETE FROM viewed_gender; DELETE FROM viewed_product_size; DELETE FROM viewed_product_type;
 DELETE FROM viewed_promos; viewed_sub_category; DELETE FROM viewed_sub_sub_category; DELETE FROM viewed_type; DELETE FROM sessions; DELETE FROM products;
+DELETE FROM profiles; 
 "

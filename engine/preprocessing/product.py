2910a8119e8b0c68073ed1c28bc653eafbeac977
Bartbabart <Bart.de.vries@outlook.com>
2020-04-07 15:37:25 +0200

    finished preproces product

--- engine/preprocessing/product.py
+++ engine/preprocessing/product.py
@@ -3,21 +3,20 @@ from mysql.connector import Error
 
 
 def queryproductdata(table_name, query, limit1, limit2, limit3):
-    sql_select_query = 'select ' + query + ' from ' + table_name + ' where ' + limit1 + ' and ' + limit2 + ' and ' + limit3
+    sql_select_query = 'select ' + query + ' from ' + table_name + ' where ' + limit1 + ' and ' + limit2 + ' and ' +\
+                       limit3
     cursor = connection.cursor()
     cursor.execute(sql_select_query)
     records = cursor.fetchall()
     return records
 
 
-# TODO: Change var naming to global names
-# update_data(column, value,
-def update_color(new_color, limit1):
-    if 'None' in new_color:
-        new_color = new_color.replace('None', 'NULL')
-        sql_update_color = 'update products set ' + new_color + ' where ' + limit1
+def update_value(value, kolom,  limit1):
+    if value is None:
+        value = 'NULL'
+        sql_update_color = 'update products set ' + kolom + ' = ' + value + ' where ' + limit1.replace('"', '\"')
     else:
-        sql_update_color = 'update products set ' + new_color + ' where ' + limit1
+        sql_update_color = 'update products set ' + kolom + ' =  "' + value.capitalize() + '" where ' + limit1.replace('"', '\"')
     cursor = connection.cursor()
     cursor.execute(sql_update_color)
 
@@ -34,7 +33,7 @@ connection = mysql.connector.connect(host='78.46.250.83',
                                      password='Bierkeet42069!')
 
 # create color set
-colorset = {'kleur'}
+colorset = {'color_set'}
 for color in queryproductdata('products', 'color', 'color != ""', 'color != ""', 'color != ""'):
     colorset.add(color[0].lower())
 
@@ -44,18 +43,22 @@ for category in queryproductdata('products', 'sub_sub_category', 'sub_sub_catego
                                  'sub_sub_category != ""'):
     categoryset.add(category[0].lower())
 
+print('updating color...')
 # update color by product_id
 for product in queryproductdata('products', 'product_id', 'color = ""', 'color = ""', 'color = ""'):
-    update_color('color = ' + str(search_data(product[0], colorset)), 'product_id = ' + '"' + product[0] + '"')
-
+    update_value(search_data(product[0], colorset), 'color', 'product_id = """' + product[0] + '"""')
+    
 # update color by name
 for name in queryproductdata('products', 'name', 'color = ""', 'name != ""', 'name != ""'):
-    update_color('color = ' + str(search_data(name[0], colorset)), 'name = "' + name[0] + '"')
+    update_value(search_data(name[0], colorset), 'color', 'name = """' + name[0] + '"""')
 
 # update sub_sub_category by name
+print('updating sub_sub_category...')
 for name in queryproductdata('products', 'name', 'sub_sub_category = ""', 'name != ""', 'name != ""'):
-    search_data(name, categoryset)
+    update_value(search_data(name[0], categoryset), 'sub_sub_category', 'name = """' + name[0] + '"""')
 
 # update sub_sub_category by description
-for description in queryproductdata('products', 'description', 'sub_sub_category = ""', 'description != ""', 'description != ""'):
-    search_data(description, categoryset)
+for description in queryproductdata('products', 'description', 'sub_sub_category = ""', 'description != ""',
+                                    'description != ""'):
+    update_value(search_data(description[0], categoryset), 'sub_sub_category', 'description = """' + description[0]
+                 + '"""')
